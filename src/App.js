95b20be95656f6e280a850c1ae38952880f584d9
HeeYeonKim98 <wow9896@naver.com>
2020-07-28 03:15:27 +0900

    react-movie-app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    isLoading: true,
+    movies: [],
+  };
+
+  movieList = () => {
+    const movielist = axios.get("https://yts-proxy.now.sh/list_movies.json");
+  };
+  componentDidMount() {
+    this.movieList();
+  }
+
+  render() {
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading..." : "we are ready"}</div>;
+  }
 }
-
-export default App;
